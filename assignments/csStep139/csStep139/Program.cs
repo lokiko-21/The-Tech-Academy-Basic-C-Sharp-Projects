90df17a2082afa5a7a70d1e4b464cd8175eef134
marco breton <marcoleal21@outlook.com>
2023-06-12 18:07:51 -0700

    created basic C# file
    finished step 139 of C# course

---
+++ assignments/csStep139/csStep139/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
