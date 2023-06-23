beed2e7cc78b21e522ef78f7b46f34b3d3bf3313
marco breton <marcoleal21@outlook.com>
2023-06-22 17:30:47 -0700

    completed assignment on step 242 for c# course

---
+++ assignments/csStep233/csStep233/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep233
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //FIRST COUPLE OF STRINGS TO CONCATENATE
+            string one = "Hi! ";
+            string two = "I'm ";
+            string three = "Marco.";
+
+            Console.WriteLine(one + two + three);  //CONCATENATING STRINGS USING THE PLUS SIGNS
+            Console.ReadLine();
+
+            string bigOne = "this is written in lowercase.";
+            bigOne = bigOne.ToUpper();  //CONVEERTING TO UPPERCASE USING .ToUpper()
+
+            Console.WriteLine(bigOne);
+            Console.ReadLine();
+
+            //CREATING A SMALL PARAGRAPH ON SENTENCE AT A TIME
+            StringBuilder paragraph = new StringBuilder();
+            paragraph.Append(one + two + three);  //EACH APPEND ADDS ON TO PARAGRAPH
+            paragraph.Append("\nAnd " + bigOne);  //EACH APPEND ADDS ON TO PARAGRAPH
+            paragraph.Append("\nAll by using StringBuilder!");
+
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ assignments/csStep242/csStep242/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep242
+{
+    class Program
+    {
+        static void Main()
+        {
+            //CREATING MY STRING AND INTEGER ARRAYS
+            string[] stringArray = new string[] {"green", "blue", "red", "yellow"};
+            int[] intArray = new int[] {7, 21, 17, 38};
+
+
+            //ASKING USER TO SELECT AN INDEX FROM STRING ARRAY
+            Console.WriteLine("Give a number to choose a string from my array: ");
+            int stringIndex = Convert.ToInt32(Console.ReadLine());
+
+            if (stringIndex <= 3)
+            {
+                Console.WriteLine(stringArray[stringIndex]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("That index does not exist.");
+                Console.ReadLine();
+            }
+
+
+            //ASKING USER TO SELECT AN INDEX FROM INTEGER ARRAY
+            Console.WriteLine("Give a number to choose a string from my array: ");
+            int integerIndex = Convert.ToInt32(Console.ReadLine());
+
+            //CHECKS TO SEE IF CHOSEN INDEX EXISTS
+            if (integerIndex <= 3)
+            {
+                Console.WriteLine(intArray[integerIndex]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("That index does not exist.");
+                Console.ReadLine();
+            }
+
+
+            //CREATING LIST OF STRINGS
+            List<string> stringList = new List<string>();
+            stringList.Add("first string");
+            stringList.Add("second string");
+            stringList.Add("third string");
+            stringList.Add("fourth string");
+            stringList.Add("fifth string");
+
+            //ASKING USER TO SELECT AN INDEX FROM STRING LIST
+            Console.WriteLine("give number to choose a string from my list: ");
+            int integerList = Convert.ToInt32(Console.ReadLine());
+
+            //CHECKS TO SEE IF INDEX IS INSIDE OF THE STRING LIST
+            if (integerList <= 4)
+            {
+                Console.WriteLine(stringList[integerList]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("index does not exist.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
