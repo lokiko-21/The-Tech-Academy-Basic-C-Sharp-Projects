ea6b178e613efc9e0d3ab65585c59ae9530aaaed
marco breton <marcoleal21@outlook.com>
2023-06-30 17:31:09 -0700

    completed assignment on step 275 of c# course

---
+++ assignments/csStep275/csStep275/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep275
+{
+    public class Math
+    {
+        //METHOD WITHIN MATH CLASS THAT WILL ADD TWO NUMBERS GIVEN userInput
+        public static void AddTwo(int userInput)
+        {
+            int sum = userInput + 50;  //ADDS 50 TO userInput
+            Console.WriteLine(sum);
+        }
+        //METHOD WITHIN MATH CLASS THAT WILL SUBTRACT TWO NUMBERS GIVEN userInput
+        public static void SubtractTwo(int userInput)
+        {
+            int difference = userInput - 1;  //SUBTRACTS 1 TO userInput
+            Console.WriteLine(difference);
+        }
+        //METHOD WITHIN MATH CLASS THAT WILL MULTIPLY TWO NUMBERS GIVEN userInput
+        public static void MultiplyTwo(int userInput)
+        {
+            int product = userInput * 10;  //MULTIPLIES userInput BY 10
+            Console.WriteLine(product);
+        }
+    }
+}
---
+++ assignments/csStep275/csStep275/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep275
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ASKING USER FOR A NUMBER
+            Console.WriteLine("Give me a number, and I will do math with it!");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Math.AddTwo(userInput);  //DOES THE AddTwo() METHOD WITH userInput
+            Math.SubtractTwo(userInput);  //DOES THE SubtractTwo() method with userInput
+            Math.MultiplyTwo(userInput);  //DOES THE MultiplyTwo() method with userInput
+
+            //RETURNS ANSWERS
+            Console.ReadLine();
+        }
+    }
+}
