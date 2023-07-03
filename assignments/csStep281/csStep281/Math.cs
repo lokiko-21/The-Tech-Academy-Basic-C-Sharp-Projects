39549ebcbc12ec1aaf2d00d8b192407f8886f348
marco breton <marcoleal21@outlook.com>
2023-07-03 15:22:57 -0700

    completed assignment on step 281 for c# course

---
+++ assignments/csStep281/csStep281/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep281
+{
+    public class Math
+    {
+        public static void Addition(int num1 = 1, int num2 = 2)
+        {
+            int sum = num1 + 5;
+            Console.WriteLine(num1 + " plus 5 is " + sum);
+            Console.WriteLine("2nd Parameter Value: " + num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignments/csStep281/csStep281/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math Add = new Math();  //INSTANTIATED THE CLASS
+
+            Math.Addition();  //CALLS METHOD WITH TWO GIVEN VALUES
+
+            Console.WriteLine("What's your favorite number?");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num1 = 21;
+            Math.Addition(num1, num2);  //CALLS METHOD BY SPECIFYING BOTH NUMBERS NOW
+        }
+    }
+}
