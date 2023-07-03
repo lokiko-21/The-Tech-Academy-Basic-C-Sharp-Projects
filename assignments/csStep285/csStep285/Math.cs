d0be8417964491f4f63a51a6ece7d3b29d819b97
marco breton <marcoleal21@outlook.com>
2023-07-03 16:30:04 -0700

    completed assignment on step 285 for c# course

---
+++ assignments/csStep285/csStep285/Math.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep285
+{
+    public static class Math  //DECLARING CLASS TO BE STATIC
+    {
+        public static void DivideByTwo(int number)  //MADE A VOID METHOD AND ADDED STATIC WHEN I MADE THE WHOLE CLASS STATIC
+        {
+            //SIMPLE CALCULATION
+            int result = number / 2;
+            Console.WriteLine("result of dividing {0} by 2 is: {1}", number, result);
+            Console.ReadLine();
+        }
+        public static void GetCalculations(int a, int b, out int sum, out int difference)  //MADE A VOID METHOD AND ADDED STATIC WHEN I MADE THE WHOLE CLASS STATIC
+        {
+            //SIMPLE CALCULATIONS
+            sum = a + b;
+            difference = a - b;
+            Console.WriteLine("{0} + {1} = {2}", a, b, sum);
+            Console.WriteLine("{0} - {1} = {2}", a, b, difference);
+            Console.ReadLine();
+        }
+        //OVERLOAD METHOD THAT HAS THE SAME NAME BUT DOES MULTIPLICATION INSTEAD OF ADDITION AND SUBTRACTION
+        public static void GetCalculations(int c, int d, out int product)  //MADE A VOID METHOD AND ADDED STATIC WHEN I MADE THE WHOLE CLASS STATIC
+        {
+            product = c * d;
+            Console.WriteLine("{0} * {1} = {2}", c, d, product);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignments/csStep285/csStep285/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep285
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number: ");  //ASKING USER FOR A NUMBER
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //Math math = new Math();  NO LONGER NEEDED SINCE WE MADE THE CLASS STATIC
+            Math.DivideByTwo(userInput);  //WILL DIVIDE THE USERS NUMBER BY 2
+
+            int a = 10;
+            int b = 5;
+            int sum;
+            int difference;
+            Math.GetCalculations(a, b, out sum, out difference);  //METHOD GIVES OUTPUT USING OUTPUT PARAMETERS
+
+            int c = 4;
+            int d = 6;
+            int product;
+            Math.GetCalculations(c, d, out product);  //OVERLOADED METHOD
+        }
+    }
+}
