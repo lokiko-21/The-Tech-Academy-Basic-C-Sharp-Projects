e76739f0f7a5f5114c13afba2f49e2b9584bcaf2
marco breton <marcoleal21@outlook.com>
2023-07-03 14:55:45 -0700

    completed assignment on step 280 of c#  course

---
+++ assignments/csstep280/csstep280/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csstep280
+{
+    public class Math
+    {
+        public static void Multiply(int num1, int times = 1)  //SETTING UP PARAMETERS FOR MY METHOD SECOND ONE IS OPTIONAL IF USER WANTS TO MULTIPLY OR NOT
+        {
+            int product = num1 * times;  //PERFORMS MATH OPERATION WITH BOTH PARAMETERS GIVEN
+            Console.WriteLine("The  number " + num1 + ", added to itself " + times + " times, is " + product);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignments/csstep280/csstep280/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csstep280
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math.Multiply(num1: 1234, times: 0);  //CALLING MY METHOD USING MY OWN PRE-SET NUMBERS
+
+            Console.WriteLine("Give me a number: ");  //ASKING USER FOR THE FIRST NUMBER
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("give me a second number: ");  //ASKING USER FOR THE SECOND NUMBER
+            int times = Convert.ToInt32(Console.ReadLine());
+
+            Math.Multiply(num1, times);  //CALLS MY METHOD USING THE PARAMETERS GIVEN BY THE USER
+
+            Math.Multiply(num1);  //CALLS MY METHOD USING ONLY ONE PARAMETER GIVEN BY THE USER
+        }
+    }
+}
