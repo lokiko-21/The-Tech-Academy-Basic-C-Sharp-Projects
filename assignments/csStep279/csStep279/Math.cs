3dd15018ce4d5a8396d1f8c9503a11f577c34b0f
marco breton <marcoleal21@outlook.com>
2023-07-03 14:01:34 -0700

    completed assignment on step 279 of c# course

---
+++ assignments/csStep279/csStep279/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep279
+{
+    public class Math
+    {
+        public static void AddTwo(int num1)  //FIRST METHOD THAT TAKES IN AN INTEGER
+        {
+            int sum = num1 + 10;  //PERFORMS MATH OPERATION ON NUMBER
+            Console.WriteLine(sum);
+        }
+        public static void AddTwo(double num2)  //SECOND METHOD WITH SAME NAME THAT TAKES IN A DECIMAL NUMBER
+        {
+            double sum = num2 + 10.1;  //PERFORMS MATH OPERATION ON NUMBER
+            Console.WriteLine(sum);
+        }
+        public static void AddTwo(string age)  //THIRD METHOD WITH SAME NAME THAT TAKES IN A STRING
+        {
+            int newAge = Convert.ToInt32(age) + 5;  //CONVERTS STRING INTO AN INTEGER AND PERFORMS MATH OPERATION ON IT
+            Console.WriteLine("in 5 years you will be {0} years old!", newAge);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignments/csStep279/csStep279/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep279
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 21;
+            Math.AddTwo(num1);  //CALLS ON FIRST METHOD THAT CALLS INTEGER
+            Console.ReadLine();
+
+            double num2 = 2.1;
+            Math.AddTwo(num2);  //CALLS ON SECOND METHOD WITH SAME NAME THAT TAKES IN DOUBLE
+            Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+            Math.AddTwo(age);  //CALLS ON THIRD METHOD WITH SAME NAME THAT TAKES IN STRING
+
+            Console.ReadLine();
+        }
+    }
+}
