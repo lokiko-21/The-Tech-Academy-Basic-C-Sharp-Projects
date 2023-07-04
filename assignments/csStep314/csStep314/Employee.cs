e2cba8f65559dd356d30a42efa4bdf6e34d3bcc3
marco breton <marcoleal21@outlook.com>
2023-07-04 14:07:00 -0700

    completed assignment on step 314 for c# course

---
+++ assignments/csStep314/csStep314/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep314
+{
+    public class Employee
+    {
+        //properties for our Employee class
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //overload of == operator will return a boolean value and compare the employee objects' Id's
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            //if both objects contain null values they will be considered equal
+            if (employee1 is null && employee2 is null)
+            {
+                return true;
+            }
+            //if only one is null but not the other then they are not equal
+            if (employee1 is null || employee2 is null)
+            {
+                return false;
+            }
+            //will return true or false depending on whether the employees Id's are equal
+            return employee1.Id == employee2.Id;
+        }
+        //overload of != operator will return a boolean value and compare the employee objects' Id's
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            //will return true or false depending on whether the employees Id's are not equal
+            return !(employee1 == employee2);
+        }
+    }
+}
---
+++ assignments/csStep314/csStep314/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first employee
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Marco";
+            employee1.LastName = "Breton";
+            employee1.Id = 1001;
+
+            //second employee
+            Employee employee2 = new Employee();
+            employee2.FirstName = "Max";
+            employee2.LastName = "Lopez";
+            employee2.Id = 1002;
+
+            //checks to see if they are the same person by checking the employees Id's
+            bool isTheSame = employee1 == employee2;
+
+            Console.WriteLine(isTheSame);
+            Console.ReadLine();
+
+            //checks to see if they are a different person by checking the employees Id's
+            bool isNotSame = employee1 != employee2;
+
+            Console.WriteLine(isNotSame);
+            Console.ReadLine();
+        }
+    }
+}
