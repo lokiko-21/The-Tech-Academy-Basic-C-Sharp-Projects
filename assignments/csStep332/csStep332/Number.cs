71d7e4baf32d286f50eca3ea400d6601c77177bf
marco breton <marcoleal21@outlook.com>
2023-07-05 11:02:16 -0700

    completed assignment on step 332 for c# course

---
+++ assignments/csStep332/csStep332/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep332
+{
+    //made a struct called Number
+    public struct Number
+    {
+        //gave Number a decimal property Amount
+        public decimal Amount { get; set; }
+    }
+}
---
+++ assignments/csStep332/csStep332/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep332
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating new number object
+            Number number = new Number();
+            //giving number.Amount a value of 1.5 (don't forget to add the M at the end)
+            number.Amount = 1.5M;
+
+            //printing it out to the console
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
