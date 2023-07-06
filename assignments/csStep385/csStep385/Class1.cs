164a689b36d27f64d5b512b5dab7c674f2013dd9
marco breton <marcoleal21@outlook.com>
2023-07-06 15:54:53 -0700

    completed assignment on step 385 for c# course

---
+++ assignments/csStep385/csStep385/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep385
+{
+    public class Class1
+    {
+        //class properties
+        public string name { get; set; }
+        public int age { get; set; }
+
+        //first constructor takes
+        public Class1(string name) : this(name, 0)
+        {
+        }
+
+        public Class1(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void Action()
+        {
+            Console.WriteLine("Name: {0} \nAge: {1} \n", name, age);
+        }
+    }
+}
---
+++ assignments/csStep385/csStep385/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStep385
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating my const variable another variable using var
+            const string creatorName = "Marco Breton";
+            var myAge = 21;
+
+            //using first contructor with only one parameter
+            Class1 object1 = new Class1("Duke");
+            object1.Action();  //calling my Action() method
+
+            //using second constructor with two parameters
+            Class1 object2 = new Class1("Lucci", 3);
+            object2.Action();  //calling my Action() method
+
+            Console.ReadLine();
+        }
+    }
+}
