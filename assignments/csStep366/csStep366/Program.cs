dd8f2d26b75f1551a54afb18217f209566f71c48
marco breton <marcoleal21@outlook.com>
2023-07-06 12:43:00 -0700

    completed assignment on step 366 for c# course

---
+++ assignments/csStep366/csStep366/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;  //using the System.IO namespace
+
+namespace csStep366
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a number: ");  //asking user for input
+            string userInput = Console.ReadLine();  //storing user input into variable
+            //using the StreamWriter class to allow me to write text in a separate file
+            using (StreamWriter file = new StreamWriter(@"log.txt", true))  //appends user input everytime to bin\Debug\log.txt
+            {
+                file.WriteLine(userInput);  //inserting new line within text file
+            }
+        }
+    }
+}
